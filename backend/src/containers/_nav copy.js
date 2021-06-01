ddf97bea8b8af48c946c1fafc573152ffe0a51d8
chanakafit <chanakasuranga@gmail.com>
2021-06-01 20:54:54 +0530

    branding and navigation changes

--- backend/src/containers/_nav copy.js
+++ backend/src/containers/_nav copy.js
@@ -6,7 +6,11 @@ export default [
         _name: 'CSidebarNavItem',
         name: 'Dashboard',
         to: '/dashboard',
-        icon: 'cil-speedometer'
+        icon: 'cil-speedometer',
+        badge: {
+          color: 'primary',
+          text: 'NEW'
+        }
       },
       {
         _name: 'CSidebarNavTitle',
@@ -197,34 +201,6 @@ export default [
         _name: 'CSidebarNavDropdown',
         name: 'Pages',
         route: '/pages',
-        icon: 'cil-star',
-        items: [
-          {
-            name: 'Login',
-            to: '/pages/login'
-          },
-          {
-            name: 'Register',
-            to: '/pages/register'
-          },
-          {
-            name: 'Error 404',
-            to: '/pages/404'
-          },
-          {
-            name: 'Error 500',
-            to: '/pages/500'
-          }
-        ]
-      },
-      {
-        _name: 'CSidebarNavTitle',
-        _children: ['Advanced']
-      },
-      {
-        _name: 'CSidebarNavDropdown',
-        name: 'User',
-        route: '/user',
         icon: 'cil-star',
         items: [
           {
